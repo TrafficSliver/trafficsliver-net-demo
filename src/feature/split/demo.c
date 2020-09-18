08843f124fdd437294316d8164e0bb974e4c50c6
Sebastian Reuter <sebastian.reuter1@rwth-aachen.de>
2020-09-18 12:28:40 +0200

    add files demo.c/.h to the build system (will contain demo code in future)

---
+++ src/feature/split/demo.c
@@ -0,0 +1,8 @@
+/**
+ * \file demo.c
+ *
+ * \brief Code for operating the Raspberry Pi demo at NetSys 2021
+ **/
+
+#include "feature/split/demo.h"
+
---
+++ src/feature/split/demo.h
@@ -0,0 +1,17 @@
+/**
+ * \file demo.h
+ *
+ * \brief Headers for demo.c
+ **/
+
+#ifndef TOR_DEMO_H
+#define TOR_DEMO_H
+
+// uncomment for enabling the demo code
+#define ENABLE_DEMO
+
+#ifdef ENABLE_DEMO
+#else /* ENABLE_DEMO */
+#endif
+
+#endif /* TOR_DEMO_H */
